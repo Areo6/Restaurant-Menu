662c249f7eaa3366dd14e5fa8dd85e793c3c2645
eubule <eubulemalaba@gmail.com>
2018-12-24 18:14:59 +0300

    (feature-admin-view-orders): Admin can view all the orders made This page allows the admin to view all the orders placed by he customers [Delivers #162116781]

--- UI/js/main.js
+++ UI/js/main.js
@@ -2,3 +2,20 @@ function order() {
     alert("You successfully placed an order. Food will be served any minute");
     return true;
 }
+var a = "inactive";
+var d = "inactive";
+
+function accepted(el) {
+    if (el.value == "Accept" && d == "inactive") {
+        el.value = "Accepted";
+        a = "active";
+    }
+    if (el.value === "Decline" && a == "inactive") {
+        el.value = "Declined";
+        d = "active";
+    }
+
+    if (el.value === "Complete" && d == "inactive" && a == "active") {
+        el.value = "Completed";
+    }
+}
